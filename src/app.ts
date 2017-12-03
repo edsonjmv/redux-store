77faf9b2f3cb3314c757f58184ef194069fe078e
edsonjmv <edsonjmv@gmail.com>
2017-12-03 17:33:05 +0100

    Store subscription

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import * as fromStore from './store';
 
-import {
-  renderTodos
-} from './utils';
+import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
@@ -10,24 +8,21 @@ const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
 const reducers = {
-  todos: fromStore.reducer
+  todos: fromStore.reducer,
 };
 
-const store = new fromStore.Store({reducers});
+const store = new fromStore.Store(reducers, {});
 
 button.addEventListener(
   'click',
   () => {
     if (!input.value.trim()) return;
 
-    const payload = {
-      label: input.value,
-      complete: false
-    };
+    const payload = { label: input.value, complete: false };
 
     store.dispatch({
       type: 'ADD_TODO',
-      payload
+      payload,
     });
 
     console.log(store.value);
@@ -37,9 +32,17 @@ button.addEventListener(
   false
 );
 
-todoList.addEventListener('click', function (event) {
+const unsubscribe = store.subscribe(state => {
+  renderTodos(state.todos.data);
+});
+
+destroy.addEventListener('click', unsubscribe, false);
+
+todoList.addEventListener('click', function(event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
     console.log(target);
   }
 });
+
+store.subscribe(state => console.log('STATE:::', state));
--- src/store/store.ts
+++ src/store/store.ts
@@ -8,17 +8,31 @@ export class Store {
   };
 
   constructor(reducers = {}, initialState = {}) {
+    this.subscribers = [];
 		this.reducers = reducers;
-    this.state = initialState;
+    this.state = this.reduce(initialState, {});
   }
 
   get value() {
     return this.state;
-	}
+  }
+  
+  subscribe(fn) {
+    this.subscribers = [...this.subscribers, fn];
+    this.notify();
+    return () => {
+      this.subscribers = this.subscribers.filter(sub => sub !== fn)
+    }
+  }
 	
 	dispatch(action) {
-		this.state = this.reduce(this.state, action);
-	}
+    this.state = this.reduce(this.state, action);
+    this.notify();
+  }
+  
+  private notify() {
+    this.subscribers.forEach(fn => fn(this.value));
+  }
 
 	private reduce(state, action) {
 		const newState = {};
