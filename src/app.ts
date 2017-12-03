cfd9c2ca8ac762e2a57453f65129fa3cd66af87a
edsonjmv <edsonjmv@gmail.com>
2017-12-03 16:41:59 +0100

    Reducers

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,43 @@
-import { renderTodos } from './utils';
+import * as fromStore from './store';
+
+import {
+  renderTodos
+} from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
+const reducers = {
+  todos: fromStore.reducer
+};
+
+const store = new fromStore.Store({reducers});
+
 button.addEventListener(
   'click',
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+    const payload = {
+      label: input.value,
+      complete: false
+    };
 
-    console.log(payload);
+    store.dispatch({
+      type: 'ADD_TODO',
+      payload
+    });
+
+    console.log(store.value);
 
     input.value = '';
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+todoList.addEventListener('click', function (event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
     console.log(target);
